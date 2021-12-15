4c4b89c700288353e4ca744ff10677c0a7b29953
John Benson <john.benson@shopify.com>
2021-12-15 19:46:48 -0400

    d14

--- d14/d14.js
+++ d14/d14.js
@@ -1,49 +1,74 @@
 const fs = require('fs');
+
 const input = fs
     .readFileSync('./d14.input', 'utf-8')
     .split(/\r?\n/)
     .filter(i => i);
 
+let initial = input[0].split('');
 
-const initialState = input[0];
+const transformers = Object.fromEntries(
+  input.slice(1, input.length).map(i => i.split(' -> ')).map(([ pair, result ]) => (
+    [ pair, [ `${pair[0]}${result}`, `${result}${pair[1]}` ] ]
+  ))
+);
 
-const transformers = Object
-  .fromEntries(input.slice(1, input.length)
-  .map(i => i.split(' -> ')));
 
-console.log(initialState, transformers);
+const getCounts = (counts, transformers) => {
+  const nextCounts = { };
 
-const step = (state, transformers) => {
-  let next = '';
+  const pairs = Object.keys(counts);
 
-  console.time("step");
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i];
 
-  state.split('').forEach(char => {
-    const previous = next.at(-1)
+    const resultPairs = transformers[pair];
 
-    if (!previous) {
-      next += char
-      return
+    if (resultPairs) {
+      nextCounts[resultPairs[0]] = (nextCounts[resultPairs[0]] || 0) + counts[pair]
+      nextCounts[resultPairs[1]] = (nextCounts[resultPairs[1]] || 0) + counts[pair]
+    } else {
+      nextCounts[pair] = (nextCounts[pair] || 0) + counts[pair]
     }
+  }
 
-    const filler = transformers[`${previous}${char}`];
-
-    if (filler) {
-      next += `${filler}${char}`
-      return
-    }
-
-    return next += char
-  });
-
-  console.timeEnd("step")
-
-  return next;
+  return nextCounts;
 }
 
-let state = initialState;
+let counts = {
 
-for (let i = 0; i < 4; i++) {
-  state = step(state, transformers)
-  console.log(state);
 }
+
+for (let i = 0; i < initial.length; i++) {
+  if (i == 0 || i === initial.length) {
+    continue;
+  }
+
+  const pair = initial[i - 1] + initial[i];
+
+  counts[pair] = (counts[pair] || 0) + 1
+}
+
+console.log(counts);
+
+const steps = 40;
+
+for (let i = 0; i < steps; i++) {
+  counts = getCounts(counts, transformers)
+}
+
+let result = {
+};
+
+//console.log(counts);
+
+Object.entries(counts).forEach(([ pair, count ]) => {
+  result[pair[1]] = (result[pair[1]] || 0) + count;
+});
+
+console.log(result)
+
+const min = Math.min(...Object.values(result))
+const max = Math.max(...Object.values(result))
+
+console.log(max - min)
